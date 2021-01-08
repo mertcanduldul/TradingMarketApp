7accc398455483464fdc0e15092132d7393f4b25
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-01-08 19:45:41 +0300

    main activityden profile veri akışı gerçekleştirildi

--- app/src/main/java/com/mertcanduldul/app/ProfileActivity.java
+++ app/src/main/java/com/mertcanduldul/app/ProfileActivity.java
@@ -1,18 +1,35 @@
 package com.mertcanduldul.app;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileActivity extends Fragment {
+    private TextView textName, textAdres;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_layout, container, false);
+        View view = inflater.inflate(R.layout.profile_layout, container, false);
+
+        textName = view.findViewById(R.id.textName);
+        textAdres = view.findViewById(R.id.textAdres);
+
+        if (getArguments()!=null){
+            String strbundle1 =this.getArguments().getString("username");
+            //String strbundle2 = bundle.getString("pwd");
+            textName.setText(strbundle1);
+            // textAdres.setText(strbundle2);
+        }
+        return view;
+
     }
 }
