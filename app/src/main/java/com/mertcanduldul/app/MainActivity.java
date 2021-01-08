9bacc6adf65cc1c9793071e3cfbdea7739e6e58f
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-01-08 19:47:28 +0300

    main classtan fragmentlere veri akışı sağlandı

--- app/src/main/java/com/mertcanduldul/app/MainActivity.java
+++ app/src/main/java/com/mertcanduldul/app/MainActivity.java
@@ -3,21 +3,30 @@ package com.mertcanduldul.app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private Fragment tempFragment;
+    private List<Urun> urunList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String usr = getIntent().getStringExtra("username");
 
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -28,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.profileActionId) {
                     tempFragment = new ProfileActivity();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("username", usr);
+                    tempFragment.setArguments(bundle);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout, tempFragment).commit();
+
                 }
                 if (item.getItemId() == R.id.searchActionId) {
                     tempFragment = new SearchActivity();
@@ -39,5 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+
     }
 }
